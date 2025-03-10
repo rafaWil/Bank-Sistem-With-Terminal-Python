2514c9f87bc771f6ba7ef3433ee83f956297b03b
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-10 16:36:12 -0300

    feat: Validating entered date format

--- files/BankingSystem.py
+++ files/BankingSystem.py
@@ -5,3 +5,11 @@ from datetime import datetime # Data
 # Funçao para Data em formato xx/xx/xxxx
 def data_formatada(data):
     return data.strftime('%d/%m/%Y')
+
+# Validando formato de data inserida
+def validar_data(data):
+    try:
+        datetime.strptime(data, '%d/%m/%Y')
+        return True
+    except ValueError:
+        return False # Returning False if input the wrong date format
